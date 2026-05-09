e621e8581125ba07835d1f0ad5d45a82a6646d56
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-05-09 08:34:19 +0600

    Coordinates of a point

---
+++ Codeforces/Coordinates_of_a_Point/Coordinates_of_a_Point.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float x, y;
+    cin >> x >> y;
+    if (x==0 && y==0)
+    {
+        cout << "Origem" ;
+        return 0;
+    }
+    
+    if (x>0)
+    {
+        if(y>0) cout << "Q1";
+        else if(y==0) cout << "Eixo X";
+        else if(y<0) cout << "Q4";
+    }
+    else if (x<0)
+    {
+        if(y>0) cout << "Q2";
+        else if(y==0) cout << "Eixo X";
+        else if(y<0) cout << "Q3";
+    }
+    else if (y!=0 && x==0) cout << "Eixo Y";
+    return 0;
+}
