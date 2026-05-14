53930e0d3d77f83b82f16922b83969e9760fb1f3
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-05-14 18:24:56 +0600

    Two intervals

---
+++ Codeforces/Two_intervals/Two_intervals.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int l1, l2, r1, r2;
+    cin >> l1 >> r1 >> l2 >> r2 ;
+    if (l2>=l1 && l2<=r1)
+    {
+        cout << l2 << " ";
+        if (r2>=l1 && r2<=r1) cout << r2;
+        else cout << r1;
+    }
+    else if (l1>=l2 && l1<=r2)
+    {
+        cout << l1 << " ";
+        if (r1>=l2 && r1<=r2) cout << r1;
+        else cout << r2;
+    }
+    else cout << "-1" ;
+    return 0;
+}
---
+++ Codeforces/Two_intervals/readme.md
@@ -0,0 +1,2 @@
+# Two intervals
+## Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/X
