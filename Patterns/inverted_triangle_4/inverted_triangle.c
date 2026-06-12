17a5af5271f7e0473cef9d8187ed196dcaa1b1f3
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-06-13 00:27:53 +0600

    Inverted Triangle 4

---
+++ Patterns/inverted_triangle_4/inverted_triangle.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < (n-i-1); j++)
+        {
+            printf("  ");
+        }
+        for (int j = 0; j <= i  ; j++)
+        {
+            printf("%d ", j+1);
+        }
+        for (int j = i ; j >=1 ; j--)
+        {
+            printf("%d ", j );
+        }
+        printf("\n");
+    }
+    
+}
---
+++ Patterns/inverted_triangle_4/readme.md
@@ -0,0 +1,14 @@
+# Inverted Triangle Pattern (Type 4)
+
+```text
+n = 3 
+    1 
+  1 2 1 
+1 2 3 2 1
+
+n = 4 
+      1 
+    1 2 1 
+  1 2 3 2 1 
+1 2 3 4 3 2 1 
+```
