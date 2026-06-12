d8a74b18be83c96929f5bb56b4a8ae2cd1d9f416
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-06-12 06:27:39 +0600

    Triangle_pattern_5

---
+++ Patterns/triangle_pattern_5/readme.md
@@ -0,0 +1,12 @@
+# Triangle Pattern (Type 1)
+
+n = 3 <br>
+a <br>
+b c <br>
+d e f <br>
+
+n = 4 <br>
+a <br>
+a b <br>
+a b c <br>
+a b c d <br>
---
+++ Patterns/triangle_pattern_5/triangle_pattern_5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    char c='a';
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i+1 ; j++)
+        {
+            printf("%c ",c);
+            c++ ;
+        }
+        printf("\n");
+    }
+    
+}
