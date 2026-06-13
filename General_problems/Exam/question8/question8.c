05ebfe3c4992a7caee7342d0a8373a74710ec7be
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-06-13 23:45:22 +0600

    Question 8

---
+++ General_problems/Exam/question8/question8.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    while (n--)
+    {
+        int m;
+        scanf("%d", &m);
+        for (int i = 0; i < m; i++)
+        {
+            printf("*");
+        }
+        
+    }
+    
+}
