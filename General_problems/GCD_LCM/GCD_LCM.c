309f322896ac14cbb646cd7c485a71fddd3eea91
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-06-13 23:22:09 +0600

    Gcd and lcm

--- General_problems/GCD_LCM/GCD_LCM.c
+++ General_problems/GCD_LCM/GCD_LCM.c
@@ -2,18 +2,19 @@
 int main(){
     int n1, n2;
     scanf("%d %d", &n1, &n2);
-    n1 = (n1>n2)? n1 : n2 ;
-    int n3 = (n1>n2)? n2 : n1 ;
-    while (n3 != 0)
+    int backup_n1 = n1, backup_n2= n2;
+    int big = (n1>n2)? n1 : n2 ;
+    int small = (n1>n2)? n2 : n1 ;
+    while (small != 0)
     {
-        int rem = n1 % n3;
-        n1 = n3;
-        n3 = rem;
+        int rem = big % small;
+        big = small;
+        small = rem;
     }
-    int gcd = n1;
+    int gcd = big;
     printf("GCD = %d", gcd);
 
-    int lcm = (n1 * n2) / gcd;
+    int lcm = (backup_n1 * backup_n2) / gcd;
     printf("\nLCM = %d", lcm);
 }
 
