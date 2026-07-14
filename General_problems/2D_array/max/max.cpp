f02a71ad94ee50f45f6193c8e61204aa74d16dd7
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-07-14 22:51:45 +0600

    maximum element in 2D array

---
+++ General_problems/2D_array/max/max.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int r, c;
+    cin >> r >> c;
+    int a[r][c];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    int mx = a[0][0];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            if (a[i][j] > mx)
+            {
+                mx = a[i][j];
+            }
+        }
+    }
+    cout << "Maximum = " << mx;
+}
