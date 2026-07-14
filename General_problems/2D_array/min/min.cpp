b857ad19cde72761cbdc6a9423439afb76cc5fee
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-07-14 22:59:56 +0600

    Minimum element in 2d array

---
+++ General_problems/2D_array/min/min.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int r, c;
+    cin >> r >> c;
+    int a[r][c];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    int mn = a[0][0];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            if (a[i][j] < mn)
+            {
+                mn = a[i][j];
+            }
+        }
+    }
+    cout << "Minimum = " << mn;
+}
