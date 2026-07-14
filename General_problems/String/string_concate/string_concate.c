5be2a41130b21985d30465e30d466009a74b4966
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-07-15 01:34:14 +0600

    String concate  without using strcat

---
+++ General_problems/String/string_concate/string_concate.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+// strcat(1st string, 2nd string);
+void main()
+{
+    int i=0, j=0, size;
+    printf("String Size : ");
+    scanf("%d",&size);
+
+    char a[size*2], b[size];
+    
+    printf("1st String : ");
+    getchar();
+    fgets(a, size*2, stdin);
+
+    printf("2nd String : ");
+    fgets(b, size, stdin);
+
+    while (a[i] != '\0' && a[i] != '\n') {
+        i++;
+    }
+    j = 0;
+    while(b[j] != '\0'){
+        a[i] = b[j];
+        i++;
+        j++;
+    }
+    a[i] = '\0';
+    printf("New String : %s", a);
+}
