6540587b83a66b1157fd768c963e06375cb6a4f8
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-07-25 05:42:24 +0600

    Let's use getline

---
+++ Codeforces/Lets_use_getline/Lets_use_getline.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string s;
+    getline(cin, s);
+    int i = 0;
+    while (s[i] != '\\')
+    {
+        cout << s[i];
+        i++;
+    }
+      
+}
---
+++ Codeforces/Lets_use_getline/readme.md
@@ -0,0 +1,2 @@
+# Let's Use Getline
+# Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/B
