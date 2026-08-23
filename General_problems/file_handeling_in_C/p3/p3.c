b17162bbc852a9f462f073ee2ebaaaa8533e9b06
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-08-23 16:50:53 +0600

    C file handeling Practise 3: Write in the file

---
+++ General_problems/file_handeling_in_C/p3/p3.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    FILE *readme = fopen("readme.md", "w");
+    fputs("# This file content is written using C code\n", readme);
+    fputs("<em>Now, I can write anything without entering the readme file</em>\n", readme);
+}
---
+++ General_problems/file_handeling_in_C/p3/readme.md
@@ -0,0 +1,2 @@
+# This file content is written using C code
+<em>Now, I can write anything without entering the readme file</em>
