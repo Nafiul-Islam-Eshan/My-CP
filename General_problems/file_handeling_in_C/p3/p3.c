e3f62a60845cc76608b075cd6fe0570909483330
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-08-23 17:01:06 +0600

    Write using user input

--- General_problems/file_handeling_in_C/p3/p3.c
+++ General_problems/file_handeling_in_C/p3/p3.c
@@ -1,7 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
     FILE *readme = fopen("readme.md", "w");
+
     fputs("# This file content is written using C code\n", readme);
     fputs("<em>Now, I can write anything without entering the readme file</em>\n", readme);
+
+    char str[400];
+    fgets(str, 400, stdin);
+    fputs(str, readme);
+
+    fclose(readme);
 }
--- General_problems/file_handeling_in_C/p3/readme.md
+++ General_problems/file_handeling_in_C/p3/readme.md
@@ -1,2 +1,3 @@
 # This file content is written using C code
 <em>Now, I can write anything without entering the readme file</em>
+<big>This is big tag</big>
